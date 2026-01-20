5debfa46e6c5f1e72bb72c5866b1491b72900f38
H00gy <148908869+H00gy@users.noreply.github.com>
2026-01-20 18:10:36 -0500

    reputation slider setup

---
+++ Assets/scripts/reputationMeter/reputationMeter.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class reputationMeter : MonoBehaviour
+{
+    Slider reputation;
+    public float repValue = 0.75f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        reputation= GetComponent<Slider>();
+        reputation.value = repValue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        reputation.value = repValue;
+    }
+}
