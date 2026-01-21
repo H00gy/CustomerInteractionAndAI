81c4b75887d6cbc39ab6cdb77c66a1eadf0bbd06
H00gy <148908869+H00gy@users.noreply.github.com>
2026-01-21 17:03:11 -0500

    figuring out price

--- Assets/scripts/ItemBrandData.cs
+++ Assets/scripts/ItemBrandData.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using System.Collections.Generic;
 
 [CreateAssetMenu(fileName = "BrandData", menuName = "brands/BrandData")]
-public class ItemBrandData : MonoBehaviour
+public class ItemBrandData : ScriptableObject
 {
     public Sprite objectShape;
     public Vector2 priceRange; 
