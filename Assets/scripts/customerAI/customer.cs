46f4ee7d600ba6c5c7f598f7692d66644721e3c8
H00gy <148908869+H00gy@users.noreply.github.com>
2026-01-26 19:18:01 -0500

    working with haggle

--- Assets/scripts/customerAI/customer.cs
+++ Assets/scripts/customerAI/customer.cs
@@ -4,6 +4,7 @@ public class customer : MonoBehaviour
 {
     
     bool customerPresent;
+    public float hagglePercent;
     private void Start()
     {
         newCustomer();
@@ -13,11 +14,24 @@ public class customer : MonoBehaviour
     }
     void newCustomer()
     {
+        // sets placement
         customerPresent= true;
         //bargainMultiplier = reputationMeter.repValue;
         playEnterAnimation();
         this.transform.position = new Vector2(0f, 0.81f); // temp since I don't have anim methods done
-        
+
+        // calls buying or selling
+        bool buyOrSell = Random.value > 0.5f; // coin toss
+        if (buyOrSell)
+        {
+            Debug.Log("buy");
+            buy();
+        }
+        else
+        {
+            Debug.Log("sell");
+            sell();
+        }
     }
     void playEnterAnimation() // will fill in later with animation scripts
     {
@@ -27,4 +41,31 @@ public class customer : MonoBehaviour
     {
 
     }
+    void buy()
+    {
+
+    }
+
+    void sell()
+    {
+        haggleChance();
+    }
+    void haggleChance()
+    {
+       hagglePercent = 1 - reputationMeter.repValue;
+       float randomValue = Random.value;
+       
+       if (randomValue < hagglePercent)
+       {
+            Debug.Log("haggle engaged");
+       }
+       else
+       {
+            Debug.Log("no haggle");
+       }
+
+    }
+    
+        
+    
 }
