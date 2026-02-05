bf54dc97c33b7735b879b8f12b2211a2407eb9c3
H00gy <148908869+H00gy@users.noreply.github.com>
2026-02-05 17:57:12 -0500

    Update currency.cs

--- Assets/scripts/currency.cs
+++ Assets/scripts/currency.cs
@@ -7,12 +7,20 @@ public class currency : MonoBehaviour
     public static float currencyAmount;
     public TMP_Text currencyText;
     public float tempCurrentAmount;
+    gameOver gameEnd;
     private void Awake()
     {
         // if there are more scenes, an if statement here for the first scene only
         currencyAmount = 100f;
         
         currencyText.text = currencyAmount.ToString();
+
+        gameEnd = GameObject.FindWithTag("gameOver").GetComponent<gameOver>(); // finds in scene
+        if (gameEnd == null)
+        {
+            Debug.Log("couldn't find gameOver");
+            return;
+        }
     }
     public void SetCurrency(float amount)
     {
